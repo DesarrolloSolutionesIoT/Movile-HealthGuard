faa5d42f1bd763141546a2e0715af01396c35d9f
ZarRubio <89089678+ZarRubio@users.noreply.github.com>
2024-06-08 14:22:41 -0500

    feat: app movil

---
+++ app/src/main/java/com/caretech/careconnect/PagoRealizadoActivity.kt
@@ -0,0 +1,29 @@
+package com.caretech.careconnect
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PagoRealizadoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_pago_realizado)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnFinalizar = findViewById<Button>(R.id.btFinalizar)
+
+        btnFinalizar.setOnClickListener {
+            val intent = Intent(this, PatientMenuActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
